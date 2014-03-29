7871095254092ac03f1101260555872cfcf8c3fe
Jiyuan Zhao <zhao89@purdue.edu>
2014-03-29 15:23:28 -0400

    use subroutine, not halting when reach EE_SIZE limit

--- Camera & Sensor/Camera_Sensor_Pixy_Testbench/Camera_Sensor_Pixy_Testbench.ino
+++ Camera & Sensor/Camera_Sensor_Pixy_Testbench/Camera_Sensor_Pixy_Testbench.ino
@@ -1,57 +1,55 @@
 //Camera_Sensor_Pixy_Testbench
 
-//#include EEPROM.h
+#include <EEPROM.h>
 #include <Pixy.h>
 #include <SPI.h>
-//const int EE_SIZE = 128;
+const int EE_SIZE = 32;
+static int i = 0;
+uint16_t blocks;
+static boolean finishDump = false;
 
-Pixy pixy;
+
+Pixy pixy,dummyPixy;
 
 void setup()
 {
   Serial.begin(9600);
-  Serial.print("Camera_Sensor_Pixy_Testbench\n");
-  //Serial.print
+  Serial.println("Camera_Sensor_Pixy_Testbench\n");
+  Serial.println("Start dumping data into EEPROM");
 }
 
 void loop()
 {
-  static int i = 0;
-  uint16_t blocks;
+  for (i;i++;i<EE_SIZE){
+    writeDummyPixyToEEPROM(i);
+    delay(20);
+  }
   
-  //while (i < EE_SIZE){
-  
-  //case 1
-  pixy.blocks[i].signature = 1;
-  pixy.blocks[i].x = 159;
-  pixy.blocks[i].y = 109;
-  pixy.blocks[i].width = 61;
-  pixy.blocks[i].height = 61;
-  pixy.blocks[i].print();
-  delay(20);
-  i++;
-  
-  //case 2
-  pixy.blocks[i].signature = 1;
-  pixy.blocks[i].x = 173;
-  pixy.blocks[i].y = 114;
-  pixy.blocks[i].width = 60;
-  pixy.blocks[i].height = 61;
-  pixy.blocks[i].print();
-  delay(20);
-  i++;
-  
-  //case 3
-  pixy.blocks[i].signature = 1;
-  pixy.blocks[i].x = 146;
-  pixy.blocks[i].y = 111;
-  pixy.blocks[i].width = 70;
-  pixy.blocks[i].height = 65;
-  pixy.blocks[i].print();
-  delay(20);
-  i++;  
-  
-  //Serial.println(i);
-  //}
+  if (i == EE_SIZE - 1) {
+    finishDump = true;
+    Serial.println("Finish dumping data into EEPROM");
+}  
+
 }
-  
+
+//subroutine for setup dunmmyPixy, write data to EEPROM
+void writeDummyPixyToEEPROM(int i){
+    dummyPixy.blocks[i].signature = (int)random(1,8);
+    dummyPixy.blocks[i].x = (int)random(255);
+    dummyPixy.blocks[i].y = (int)random(255);
+    EEPROM.write(3*i,dummyPixy.blocks[i].signature);
+    EEPROM.write(3*i+1,dummyPixy.blocks[i].x);
+    EEPROM.write(3*i+2,dummyPixy.blocks[i].y);
+    
+    Serial.print("dummyPixy ");
+    Serial.print(i);
+    Serial.print(" at EEPROM location ");
+    Serial.print(3*i);
+    Serial.print(" Object Color: ");
+    Serial.print(dummyPixy.blocks[i].signature);
+    Serial.print(", X: ");
+    Serial.print(dummyPixy.blocks[i].x);    
+    Serial.print(", Y: ");
+    Serial.print(dummyPixy.blocks[i].y);
+    Serial.println();
+}
